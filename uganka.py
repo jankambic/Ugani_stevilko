3605875910676f943c4edf1047884c29ca78d717
Jan Kambič <jan.kambic@gmail.com>
2017-02-08 19:52:25 +0100

    Zaključena naloga.

---
+++ uganka.py
@@ -0,0 +1,7 @@
+skrito_stevilo = "13"
+ugani_stevilko = raw_input("Vpisi iskano stevilko, morda se ti posreci: ")
+
+if ugani_stevilko == skrito_stevilo:
+    print("Bravo, zadeli ste!")
+else:
+    print("Prosimo, poskusite ponovno.")
